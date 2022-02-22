95b570b71c75a909a80bcfdd7608f6f30e136b88
Ihor Ryabets <eronryabets@gmail.com>
2022-02-22 16:50:30 +0200

    add : DAO / Service / Controller

---
+++ src/main/java/com/eronryabets/spring/springboot/spring_course_springboot/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.eronryabets.spring.springboot.spring_course_springboot.service;
+
+import com.eronryabets.spring.springboot.spring_course_springboot.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    public List<Employee> getAllEmployees();
+
+//    public void saveEmployee(Employee employee);
+//
+//    public Employee getEmployee(int id);
+//
+//    public void deleteEmployee(int id);
+
+}
